86e16dc6b40414deed8acbde2e7acb9fffeb668e
Puneet Shukla <puneetshukla041@gmail.com>
2026-02-13 12:45:19 +0530

    .

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,28 +1,20 @@
 'use client'
 import React from 'react';
-// REMOVED: import dynamic from 'next/dynamic'; <--- DELETE THIS LINE
-
 import Header from '../../components/common/Header';
 import Footer from '../../components/common/footer';
 import Section1 from '../../components/about/Section1';
+import Section2 from '../../components/about/Section2'; // Direct Import
 
-// CHANGED: Direct import instead of dynamic. 
-// This bundles the code together so it's ready instantly.
-import Section2 from '../../components/about/Section2'; 
-
-const AboutPage = () => {
+export default function AboutPage() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-[#020617]">
       <Header />
-      
       <main className="flex-grow">
+        {/* Render both sections immediately */}
         <Section1 />
         <Section2 />
       </main>
-
       <Footer />
     </div>
   );
-};
-
-export default AboutPage;
+}
